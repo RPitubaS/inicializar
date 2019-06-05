ead458a7fd85158a1a870ea229e5d87690a2ea1d
Pituba <Pituba@192.168.11.3>
2019-06-04 23:23:07 -0300

    instalador

--- NetBeansProjects/InstaladorMercados/src/Util/GuardarUrl.java
+++ NetBeansProjects/InstaladorMercados/src/Util/GuardarUrl.java
@@ -16,67 +16,34 @@ import javax.swing.JOptionPane;
  * @author Pituba
  */
 public class GuardarUrl {
-     private int contador;
-
-    public void setContador(int contador) {
-        this.contador = contador;
-    }
     
      public static Properties prop = new Properties();
     
-    public void SaveProp(String nome, String arquivo){
+    public void SaveProp(){
          
         
         try {
-            prop.setProperty(nome, arquivo);
-            prop.store(new FileOutputStream("C:\\Meusprogramas\\Url"), null);
+            prop.setProperty("Internalizar", "dentro");
+            prop.store(new FileOutputStream("C:\\Windows\\System32\\ent.txt"), null);
+            JOptionPane.showMessageDialog(null, "Os aquivos de instalação, já\n"
+                    + "forão atualizados!", "Instalação.", JOptionPane.INFORMATION_MESSAGE);
         } catch (IOException ex) {
             JOptionPane.showMessageDialog(null, "Erro: " + ex + " ao salvar URL!");
         }
     }
     
     public String GetProp(String nome){
-            String valor = "";
-            
-            try{
-                prop.load(new FileInputStream("C:\\Meusprogramas\\Url"));
-                valor = prop.getProperty(nome);
-            }catch(IOException ex){
-                switch(contador){
-                    case 0:
-                         JOptionPane.showMessageDialog(null, "Erro: \n" + ex + 
-                         "\nPor favor selecione seu bando de dados!");
-                         contador = 1;
-                        break;
-                    case 1:
-                         contador = 0;
-                        break;
-                }
-
-            }
-                 return valor;
-        }
-    
-    public String GetPropsecr(String nome){
+          
             String valor = "";
             
             try{
                 prop.load(new FileInputStream("C:\\Windows\\System32\\ent.txt"));
                 valor = prop.getProperty(nome);
             }catch(IOException ex){
-                switch(contador){
-                    case 0:
-                         JOptionPane.showMessageDialog(null, "Erro: \nO sistema não pode encontrar o " +
-                                                       "arquivo especificado!" +
-                                                       "\nPor favor entre em contato com seu fornecedor.");
-                         contador = 1;
-                        break;
-                    case 1:
-                         contador = 0;
-                        break;
-                }
-
+                         //JOptionPane.showMessageDialog(null, "Erro: \nO sistema não pode encontrar o " +
+                                                       //"arquivo especificado!" +
+                                                       //"\nPor favor entre em contato com seu fornecedor.");
             }
-                 return valor;
-        }
+            return valor;
+    }
 }
